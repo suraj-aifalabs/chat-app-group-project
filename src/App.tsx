7af3249832a7bea4142da123f75b9b6744ebba60
nikithapajjuri <nikitha.pajjuri@aifalabs.com>
2025-04-04 16:56:26 +0530

    mainpage

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,30 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Components/Login";
 import Menu from "./Components/Menu";
 import Reserve from "./Components/Reserve";
-import "./App.css";
+// import "./App.css";
 import Navbar from "./Components/Navbar";
-import Hero from "./Components/Hero";
+//import Hero from "./Components/Hero";
 import Footer from "./Components/Footer";
 import About from "./Components/About";
+import Hero from "./Components/Hero";
 
 
 
 function App() {
   return (
-<BrowserRouter>
-<Navbar/>
-<Hero/>
-<About/>
-<Footer/>
-<Routes>
-        
-        
-        <Route path="/login" element={<Login />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/reserve" element={<Reserve />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+
+    <BrowserRouter>
+        <Navbar />
+        <Hero />
+        <About/>
+        <Footer />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/reserve" element={<Reserve />} />
+        </Routes>
+      </BrowserRouter></>
   );
 }
 
--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,8 +1,9 @@
-
+import "../Styles/About.css";
 
 function About() {
   return (
     <div className="about-container">
+      {/* <div style={{height:"150vh"}}></div> */}
           <h1>About Us</h1>
           <p>Welcome to our restaurant! We serve the best dishes with fresh ingredients, crafted with love.</p>
           <p>Our team is dedicated to providing an exceptional dining experience with a cozy ambiance.</p>
