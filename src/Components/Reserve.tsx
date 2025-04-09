89114b11dcae2b30389d920e8db68d98f8a8bc02
SandeepGujjarlapudi <sandeep.gujjarlapudi@aifalabs.com>
2025-04-09 15:07:05 +0530

    changes in reserve

--- src/Components/Reserve.tsx
+++ src/Components/Reserve.tsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import "./../styles/reserve.css";
 import { useNavigate } from "react-router-dom";
 
-import { useNavigate } from "react-router-dom";
 
 import {
   TextField,
